e3f486b64970d6c18a7e9c1ef24f37c705e62b90
Jessica K <jessikim807@gmail.com>
2020-04-08 21:02:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -128,6 +128,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-programmer_hash[:dennis_ritchie][:languges] = ["C", "Assembly"]
+programmer_hash[:dennis_ritchie][:languges] << "Assembly"
 return programmer_hash
 end
