adb598dbf2c27aa20c9741758a1a4254026f2096
Jessica K <jessikim807@gmail.com>
2020-04-08 21:02:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -128,6 +128,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-programmer_hash[:dennis_ritchie][:languges] << "Assembly"
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 return programmer_hash
 end
